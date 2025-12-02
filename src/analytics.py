75989279780d755f322e344ad6adba9cfc445131
lyna500 <lynasidane55@gmail.com>
2025-12-02 11:22:54 +0100

    Update analytics.py

--- src/analytics.py
+++ src/analytics.py
@@ -1,3 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def stats_vehicules(vehicules):
+    data = [{"marque": v.marque, "modele": v.modele, "kilometrage": v.kilometrage} for v in vehicules]
+    df = pd.DataFrame(data)
+    print("Nombre de véhicules par marque :")
+    print(df.groupby("marque").size())
+    print("Kilométrage total par modèle :")
+    print(df.groupby("modele")["kilometrage"].sum())
+
+    # Graphique simple
+    df.groupby("marque").size().plot(kind="bar", title="Véhicules par marque")
+    plt.ylabel("Nombre de véhicules")
+    plt.savefig("graphs/vehicules_par_marque.png")
+    plt.close()
 
 
 
